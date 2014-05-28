60c1bb73e2ec4e51310fb1a68a43405ba83f0a87
Mark Bradshaw <mark.bradshaw@gmail.com>
2014-05-27 22:13:43 -0400

    open_spritz rewritten

--- spritz.js
+++ spritz.js
@@ -1,332 +1,143 @@
-// spritz.js
-// A JavaScript Speed Reader
-// rich@gun.io
-// https://github.com/Miserlou/OpenSpritz
+/*
+	Copyright 2014 Mark Bradshaw (mark.bradshaw@gmail.com), Joel Fouse (jfouse@gmail.com)
+*/
 
-// Please don't abuse this.
-var readability_token = '172b057cd7cfccf27b60a36f16b1acde12783893';
-var diffbot_token = '2efef432c72b5a923408e04353c39a7c';
+(function() {
+    var open_spritz = {};
 
-function create_spritz(){
-
-     spritz_loader = function() {
-        //getURL("https://rawgithub.com/Miserlou/OpenSpritz/master/spritz.html", function(data){
-
-        //getURL("https://rawgithub.com/Miserlou/OpenSpritz/dev/spritz.html", function(data){
-
-        // This won't work in Firefox because an old bug and won't work in Chrome because of security stuff:
-        //getURL("spritz.html", function(data){
-
-        //getURL("https://rawgithub.com/Miserlou/OpenSpritz/dev/spritz.html", function(data){
-
-        // RawGit's CDN usage:
-        // "Since files are not refreshed after the first request,
-        // it's best to use a specific tag or commit URL, not a branch URL."
-        getURL("https://cdn.rawgit.com/Miserlou/OpenSpritz/9e92c605032be16c986ed699d68e0acd3534e6b1/spritz.html", function(data){
-            var spritzContainer = document.getElementById("spritz_container");
-
-            if (!spritzContainer) {
-                var ele = document.createElement("div");
-                data = data.replace(/(\r\n|\n|\r)/gm,"");
-                ele.innerHTML = data;
-                document.body.insertBefore(ele, document.body.firstChild);
-                document.getElementById("spritz_toggle").style.display = "none";
-            };
-
-            document.getElementById("spritz_selector").addEventListener("change", function(e) {
-                clearTimeouts();
-                spritz();
-            });
-        });
+    var internals = {
+        state: 'stopped',
+        wpm: 250
     };
 
-    spritz_loader();
-}
+    /******** PUBLIC *********/
 
-function getURL(url, callback) {
-    var xmlhttp = new XMLHttpRequest();
+    open_spritz.assignElement = function(el) {
+        internals.el = el;
+    };
 
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            callback(xmlhttp.responseText);
-        }
-    }
-
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
-
-function hide_spritz(){
-    document.getElementById("spritz_spacer").style.display = "none";
-    document.getElementById("spritz_container").style.display = "none";
-    document.getElementById("spritz_holder").style.display = "none";
-}
-
-// Entry point to the beef.
-// Gets the WPM and the selected text, if any.
-function spritz(){
-
-    var wpm = parseInt(document.getElementById("spritz_selector").value, 10);
-    if(wpm < 1){
-        return;
-    }
-
-    var selection = getSelectionText();
-    if(selection){
-        spritzify(selection);
-    }
-    else{
-        spritzifyURL();
-    }
-}
-
-// The meat!
-function spritzify(input){
-
-    var wpm = parseInt(document.getElementById("spritz_selector").value, 10);
-    var ms_per_word = 60000/wpm;
-
-    // Split on any spaces.
-    var all_words = input.split(/\s+/);
-
-    // The reader won't stop if the selection starts or ends with spaces
-    if (all_words[0] == "")
-    {
-        all_words = all_words.slice(1, all_words.length);
-    }
-
-    if (all_words[all_words.length - 1] == "")
-    {
-        all_words = all_words.slice(0, all_words.length - 1);
-    }
-
-    var word = '';
-    var result = '';
-
-    // Preprocess words
-    var temp_words = all_words.slice(0); // copy Array
-    var t = 0;
-
-    for (var i=0; i<all_words.length; i++){
-
-        if(all_words[i].indexOf('.') != -1){
-            temp_words[t] = all_words[i].replace('.', '&#8226;');
+    open_spritz.setWPM = function(wpm) {
+        if (wpm === 0) {
+            return;
         }
 
-        // Double up on long words and words with commas.
-        if((all_words[i].indexOf(',') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf('-') != -1 || all_words[i].indexOf('(') != -1|| all_words[i].length > 8) && all_words[i].indexOf('.') == -1){
-            temp_words.splice(t+1, 0, all_words[i]);
-            temp_words.splice(t+1, 0, all_words[i]);
-            t++;
-            t++;
+        internals.wpm = wpm;
+    };
+
+    open_spritz.spritzify = function(text) {
+        if (internals.el === undefined) {
+            console.log('need an element');
+            return;
         }
 
-        // Add an additional space after punctuation.
-        if(all_words[i].indexOf('.') != -1 || all_words[i].indexOf('!') != -1 || all_words[i].indexOf('?') != -1 || all_words[i].indexOf(':') != -1 || all_words[i].indexOf(';') != -1|| all_words[i].indexOf(')') != -1){
-            temp_words.splice(t+1, 0, " ");
-            temp_words.splice(t+1, 0, " ");
-            temp_words.splice(t+1, 0, " ");
-            t++;
-            t++;
-            t++;
+        if (text === '' || text === undefined) {
+            console.log('need some text');
+            return;
         }
 
-        t++;
+        internals.text = text;
+        internals.split_text = text.split(/\s+/);
+        internals.position = 0;
 
-    }
+        open_spritz.start();
+    };
 
-    all_words = temp_words.slice(0);
+    open_spritz.start = function() {
+        internals.state = 'running';
+        displayWord();
+    };
 
-    var currentWord = 0;
-    var running = true;
-    var spritz_timers = new Array();
+    open_spritz.stop = function() {
+        internals.state = 'stopped';
+    };
 
-    document.getElementById("spritz_toggle").addEventListener("click", function() {
-        if(running) {
-            stopSpritz();
+    open_spritz.toggle = function() {
+        if (internals.state === 'stopped') {
+            open_spritz.start();
         } else {
-            startSpritz();
+            open_spritz.stop();
         }
-    });
-
-    function updateValues(i) {
-
-        var p = pivot(all_words[i]);
-        document.getElementById("spritz_result").innerHTML = p;
-        currentWord = i;
-
-    }
-
-    function startSpritz() {
-
-        document.getElementById("spritz_toggle").style.display = "block";
-        document.getElementById("spritz_toggle").textContent = "Pause";
-
-        running = true;
-
-        spritz_timers.push(setInterval(function() {
-            updateValues(currentWord);
-            currentWord++;
-            if(currentWord >= all_words.length) {
-                currentWord = 0;
-                stopSpritz();
-            }
-        }, ms_per_word));
-    }
-
-    function stopSpritz() {
-        for(var i = 0; i < spritz_timers.length; i++) {
-            clearTimeout(spritz_timers[i]);
-        }
-
-        document.getElementById("spritz_toggle").textContent = "Play";
-        running = false;
-    }
-
-    startSpritz();
-}
-
-// Find the red-character of the current word.
-function pivot(word){
-    var length = word.length;
-
-    var bestLetter = 1;
-    switch (length) {
-        case 1:
-            bestLetter = 1; // first
-            break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            bestLetter = 2; // second
-            break;
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-            bestLetter = 3; // third
-            break;
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-            bestLetter = 4; // fourth
-            break;
-        default:
-            bestLetter = 5; // fifth
     };
 
-    word = decodeEntities(word);
-    var start = '.'.repeat((11-bestLetter)) + word.slice(0, bestLetter-1).replace('.', '&#8226;');
-    var middle = word.slice(bestLetter-1,bestLetter).replace('.', '&#8226;');
-    var end = word.slice(bestLetter, length).replace('.', '&#8226;') + '.'.repeat((11-(word.length-bestLetter)));
+    /******* PRIVATE **********/
 
-    var result;
-    result = "<span class='spritz_start'>" + start;
-    result = result + "</span><span class='spritz_pivot'>";
-    result = result + middle;
-    result = result + "</span><span class='spritz_end'>";
-    result = result + end;
-    result = result + "</span>";
-
-    result = result.replace(/\./g, "<span class='invisible'>.</span>");
-
-    return result;
-}
-
-// Get the currently selected text, if any.
-// Shameless pinched from StackOverflow.
-function getSelectionText() {
-    var text = "";
-    if (typeof window.getSelection != "undefined") {
-        var sel = window.getSelection();
-        if (sel.rangeCount) {
-            var container = document.createElement("div");
-            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                container.appendChild(sel.getRangeAt(i).cloneContents());
-            }
-            text = container.innerText || container.textContent;
+    function displayWord() {
+        if (internals.state === 'stopped') {
+            return;
         }
-    } else if (typeof document.selection != "undefined") {
-        if (document.selection.type == "Text") {
-            text = document.selection.createRange().text;
+
+        if (internals.position >= internals.split_text.length) {
+            internals.state = 'stopped';
+            return;
         }
-    }
-    if(text === ""){
-        return false;
-    }
-    else{
-        return text;
-    }
-}
 
-// Uses the Readability API to get the juicy content of the current page.
-function spritzifyURL(){
-    var url = document.URL;
+        var word = internals.split_text[internals.position++];
+        var pivot = pivotLetter(word);
 
-    //getURL("https://www.readability.com/api/content/v1/parser?url="+ encodeURIComponent(url) +"&token=" + readability_token +"&callback=?",
-    getURL("https://api.diffbot.com/v2/article?url="+ encodeURIComponent(url) +"&token=" + diffbot_token, // +"&callback=?",
-        function(data) {
+        var startPad = repeat('&nbsp;', 11 - pivot);
+        var start = word.slice(0, pivot - 1);
+        var letter = word.slice(pivot - 1, pivot);
+        var end = word.slice(pivot, word.length);
+        var endPad = repeat('&nbsp;', 11 - (word.length - pivot));
 
-            data = JSON.parse(data);
+        internals.el.innerHTML = startPad + start + '<span class="OSCursor">' + letter + '</span>' + end + endPad;
 
-            if(data.error){
-                document.getElementById("spritz_result").innerText = "Article extraction failed. Try selecting text instead.";
-                return;
+        // standard delay
+        var delay = 60 * 1000 / internals.wpm;
+
+        var lastLetter = word.slice(word.length - 1);
+        if (lastLetter === ',') {
+            delay += delay * 1.3;
+        }
+
+        // Looking for punction in the last two characters.  Could be !" or ?) or just . at the end.  We need a pause for that.
+        if (word.length > 2) {
+            var lastTwoLetters = word.slice(word.length - 2);
+            if (['?', '!', '.', ';'].indexOf(lastTwoLetters) !== -1) {
+                delay += delay * 1.8;
             }
+        }
 
-            var title = '';
-            if(data.title !== ""){
-                title = data.title + ". ";
-            }
-
-            var author = '';
-            if(data.author !== undefined){
-                author = "By " + data.author + ". ";
-            }
-
-            var body = data.text;
-            body = body.trim(); // Trim trailing and leading whitespace.
-            body = body.replace(/\s+/g, ' '); // Shrink long whitespaces.
-
-            var text_content = title + author + body;
-            text_content = text_content.replace(/\./g, '. '); // Make sure punctuation is apprpriately spaced.
-            text_content = text_content.replace(/\?/g, '? ');
-            text_content = text_content.replace(/\!/g, '! ');
-            spritzify(text_content);
-        });
-
-}
-
-//////
-// Helpers
-//////
-
-// This is a hack using the fact that browers sequentially id the timers.
-function clearTimeouts(){
-    var id = window.setTimeout(function() {}, 0);
-
-    while (id--) {
-        window.clearTimeout(id);
+        setTimeout(displayWord, delay);
     }
-}
 
-// Let strings repeat themselves,
-// because JavaScript isn't as awesome as Python.
-String.prototype.repeat = function( num ){
-    if(num < 1){
-        return new Array( Math.abs(num) + 1 ).join( this );
+    function pivotLetter(word) {
+        var bestLetter;
+        switch (word.length) {
+            case 1:
+                bestLetter = 1; // first
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                bestLetter = 2; // second
+                break;
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                bestLetter = 3; // third
+                break;
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+                bestLetter = 4; // fourth
+                break;
+            default:
+                bestLetter = 5; // fifth
+        }
+
+        return bestLetter;
     }
-    return new Array( num + 1 ).join( this );
-};
 
-function decodeEntities(s){
-    var str, temp= document.createElement('p');
-    temp.innerHTML= s;
-    str= temp.textContent || temp.innerText;
-    temp=null;
-    return str;
-}
+    function repeat(letter, num) {
+        if (num < 1) {
+            return new Array(Math.abs(num) + 1).join(letter);
+        }
+        return new Array(num + 1).join(letter);
+    }
 
-
+    window.open_spritz = open_spritz;
+})();
